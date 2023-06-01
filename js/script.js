0e49f8622c2dc2d9f9abed7a942759d9213539c2
CodiesCode <125252559+CodiesCode@users.noreply.github.com>
2023-06-01 20:15:26 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
 const DONE = 4;
-let baseUrl = "https://api.openweathermap.org/"
+const baseUrl = "https://api.openweathermap.org/"
+const apiKey = "676c72c916e72c31ba00527baf05ec4e"
 
 const getLatLongUrl = (searchTerm) => {
-  return `${baseUrl}geo/1.0/direct?q=${searchTerm}&limit=1&appid=676c72c916e72c31ba00527baf05ec4e`;
+  return `${baseUrl}geo/1.0/direct?q=${searchTerm}&limit=1&appid=${apiKey}`;
 }
 
 const getWeatherDataUrl = (lat, lon) => {
-  return `${baseUrl}data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=676c72c916e72c31ba00527baf05ec4e`;
+  return `${baseUrl}data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
 }
 
 const makeRequest = (url) => {
@@ -39,17 +40,17 @@ const makeRequest = (url) => {
 async function submitForm(event) {
   event.preventDefault(); // Prevent form from submitting and refreshing the page
   
-  let container = document.getElementById("container");
+  const container = document.getElementById("container");
 
-  let errorElement = document.createElement("p");
+  const errorElement = document.createElement("p");
   if (errorElement !== null) {
     errorElement.remove();
   }
   errorElement.className = "error";
   errorElement.textContent = "The site you’re looking for cannot be found. This might be due to our servers being down or that the city you’re searching for doesn’t exist in our database."
   
-  let inputElement = document.getElementById("input-text");
-  let searchTerm = inputElement.value;
+  const inputElement = document.getElementById("input-text");
+  const searchTerm = inputElement.value;
 
   let box = document.getElementById("box");
   if (box !== null) {
@@ -60,44 +61,44 @@ async function submitForm(event) {
   container.appendChild(box);
 
   try {
-    let getlatLong = await makeRequest(getLatLongUrl(searchTerm));
-    let { lat, lon } = JSON.parse(getlatLong)[0];
+    const getlatLong = await makeRequest(getLatLongUrl(searchTerm));
+    const { lat, lon } = JSON.parse(getlatLong)[0];
 
-    let getWeatherData = await makeRequest(getWeatherDataUrl(lat, lon));
-    let weatherData = JSON.parse(getWeatherData);
+    const getWeatherData = await makeRequest(getWeatherDataUrl(lat, lon));
+    const weatherData = JSON.parse(getWeatherData);
 
-    let temp = weatherData.main.temp;
-    let windSpeed = weatherData.wind.speed;
-    let weatherDescription = weatherData.weather[0].description;
-    let iconSrc = `https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@4x.png`;
+    const temp = weatherData.main.temp;
+    const windSpeed = weatherData.wind.speed;
+    const weatherDescription = weatherData.weather[0].description;
+    const iconSrc = `https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@4x.png`;
 
-    let searchTermElement = document.createElement("p");
+    const searchTermElement = document.createElement("p");
     searchTermElement.className = "search-term";
     searchTermElement.textContent = searchTerm.toUpperCase();
     box.appendChild(searchTermElement);
 
-    let descriptionElement = document.createElement("p");
+    const descriptionElement = document.createElement("p");
     descriptionElement.className = "description";
     descriptionElement.textContent = weatherDescription.charAt(0).toUpperCase() + weatherDescription.slice(1);
     box.appendChild(descriptionElement);
 
-    let iconElement = document.createElement("img");
+    const iconElement = document.createElement("img");
     iconElement.className = "temperature-icon"
     iconElement.src = iconSrc;
     box.appendChild(iconElement);
 
-    let tempElement = document.createElement("p");
+    const tempElement = document.createElement("p");
     box.appendChild(tempElement);
     tempElement.className = "temperature";
     tempElement.textContent = `Temperature: ${Math.round(temp)} C`;
 
-    let windSpeedElement = document.createElement("p")
+    const windSpeedElement = document.createElement("p")
     windSpeedElement.className = "windspeed"
     windSpeedElement.textContent = `Windspeed: ${Math.round(windSpeed)} m/s`;
     box.appendChild(windSpeedElement);
 
   } catch (error) {
-    console.log(error);
+    console.error(error);
     box.appendChild(errorElement);
   }
 }
